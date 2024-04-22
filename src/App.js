1a8188ba6be49599fa5f67f71bd45097b23ea86b
akhtar21yr <akhtar.decy@gmail.com>
2024-04-22 20:23:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import { useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 
 function App() {
-  const [mode, setMode] = useState('light');
-
-  const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      document.body.style.color = 'white';
-    } else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      document.body.style.color = 'black';
-    }
-  };
-
   return (
-    <>
-      <Navbar title="Text Utils" aboutText="About Text Utils" mode={mode} toggleMode={toggleMode} />
-      <div className="container my-3">
-        <TextForm heading="Enter your text below" mode={mode} />
-        {/* <About /> */}
-      </div>
-      {/* <Router>
-      <Navbar title="Text Utils" aboutText="About Text Utils" mode={mode} toggleMode={toggleMode} />
-      <div className="container my-3">
+    <Router>
+      <Navbar title="Text Utils" about="About" mode="light" toggleMode={() => {}} />
+      <div className="container">
         <Routes>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<TextForm heading="Enter your text below" mode={mode} />} />
+          <Route path="/" element={<TextForm heading="Enter your text below" />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
-    </Router> */}
-    </>
+    </Router>
   );
 }
 
